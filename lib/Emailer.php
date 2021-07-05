336e0ab0387a17bb0d9fe4992ea33475342a0caa
Raff <archangel.raffael@gmail.com>
2021-07-05 11:42:10 -0400

    Update Emailer.php

--- lib/Emailer.php
+++ lib/Emailer.php
@@ -3,6 +3,6 @@
 
 class Emailer
 {
- const API_KEY = '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';
- const TEMPLATE_ID = 'd-c029d1000aba48ea86513d29eb882280';
+ const API_KEY = '';
+ const TEMPLATE_ID = '';
 }
